e0211d2f301b8d74cb8c856dc77a311e197fd6bf
Samuel Simões <samuelsimoes31@gmail.com>
2020-10-19 12:03:53 -0300

    q1 só cada uma só conta 1 vez kkkkkkkk

--- q1/questao1.c
+++ q1/questao1.c
@@ -2,6 +2,49 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#define NUM_THREADS 5
+
+long contador = 0; 
+pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *threadFunc(void *threadid){
+    pthread_mutex_lock(&mymutex);
+    if(contador == 1000000) {
+        printf("t%d: perdi\n",*(int *)threadid);
+        pthread_mutex_unlock(&mymutex);
+        pthread_exit(NULL);
+    }
+    else {
+        contador++;
+        printf("t%d -> %ld\n",*(int *)threadid, contador);
+        if(contador == 1000000) {
+            puts("GANHEI PORRA\n");
+            pthread_mutex_unlock(&mymutex);
+            pthread_exit(NULL);
+        }
+        else{
+            pthread_mutex_unlock(&mymutex);
+        }
+    }
+}
+
 int main(void) {
+    pthread_t threads[NUM_THREADS];
+    int *taskids[NUM_THREADS];
+
+    int t,i,rc;  
+    for(t=0; t<NUM_THREADS; t++){      
+        taskids[t] = (int *) malloc(sizeof(int)); *taskids[t] = t;
+        printf("No main: criando thread %d\n", t);      
+        rc = pthread_create(&threads[t], NULL, threadFunc, (void *) taskids[t]);      
+        if (rc){
+            for(i=0;i<=t;i++) free(taskids[t]);       
+            printf("ERRO; código de retorno é %d\n", rc);         
+            exit(-1);      
+        }
+        pthread_join(threads[t], NULL);
+    } 
+    for(t=0; t<NUM_THREADS; t++) free(taskids[t]); 
+
     return 0;
 }
