98a5b57de014c4099beec4e1ca2f5d29424311ec
Samuel Simões <samuelsimoes31@gmail.com>
2020-10-26 10:03:05 -0300

    Q2 FEITA \o/ GLORIFICA SENHOR

--- q2/questao2.c
+++ q2/questao2.c
@@ -9,17 +9,42 @@ pthread_mutex_t *mutex_lines = NULL;
 pthread_mutex_t mutex_open_file = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_print = PTHREAD_MUTEX_INITIALIZER;
 
-void print_line(int line, char str[26], int len){
+void set_line_color(int line){
+    if(line != 6){
+        printf("\e[38;2;0;0;0m"); //seta cor do texto para preto
+        switch(line){
+            //setar cor do fundo
+            case 1: {printf("\e[48;2;255;0;0m");break;}     //texto vermelho
+            case 2: {printf("\e[48;2;255;255;0m");break;}   //texto amarelo
+            case 3: {printf("\e[48;2;0;0;255m");break;}     //texto azul
+            case 4: {printf("\e[48;2;255;0;255");break;}    //texto magenta
+            case 5: {printf("\e[48;2;0;255;0");break;}      //texto verde
+            case 7: {printf("\e[48;2;0;255;255");break;}    //texto ciano
+        }
+    } else {
+        printf("\e[38;2;255;255;255m"); //seta cor do texto para branco
+        printf("\e[48;2;0;0;0m"); //texto preto
+    }
+}
+
+void print_line(int line, char *str, int len){
     int i=len;
     pthread_mutex_lock(&mutex_print);
-    printf("\e[%d;1H",line);
-    printf("%s",str);
-    while (i++ < 20){
+    printf("\e[%d;1H",line); //seta cursor pra linha "line" e coluna 1
+    set_line_color(line);
+    printf("%s",str); //imprime código e cidade
+    while (i++ < 20){ //espaço até a posição 20
         putchar(' ');
     }
-    printf("%s",str+len+1);
-    fflush(stdout);
+    printf("%s",str+len+1); //imprime hora
+    fflush(stdout); //dá um flush na saída pois estava havendo alguns bugs
     pthread_mutex_unlock(&mutex_print);
+}
+
+void clean_screen(int show_cursor){
+    printf("\e[H"); //seta cursor pra posição inicial
+    printf("\e[J"); //limpa tudo a partir do cursor
+    printf("\e[?25%c",show_cursor?'h':'l'); //(?25h exibe, ?25l esconde) o cursor
 }
 
 void *thread_func(){
@@ -48,14 +73,13 @@ void *thread_func(){
 
             fscanf(file," %[^\n]",string);
             len = strlen(string)-6;
-            string[len] = 0;
+            string[len] = 0; //setar o fim da string na posição antes do horário
             print_line(line,string,len);
             sleep(2);
 
             pthread_mutex_unlock(&mutex_lines[line-1]);
         }
         
-
         fclose(file);
         file = NULL;
         pthread_mutex_lock(&mutex_open_file);
@@ -67,18 +91,14 @@ void *thread_func(){
 int main(void) {
     int i,rc;
     pthread_t *threads = NULL;
-    printf("\e[H"); //seta cursor pra posição inicial
-    printf("\e[J"); //limpa tudo a partir do cursor
+    clean_screen(1);
     printf("Número de arquivos:");
     scanf("%d",&n_files);
     printf("Número de linhas:");
     scanf("%d",&n_lines);
     printf("Número de número de threads:");
     scanf("%d",&n_threads);
-    // fflush(stdin);
-    printf("\e[H"); //seta cursor pra posição inicial
-    printf("\e[J"); //limpa tudo a partir do cursor
-    printf("\e[?25l"); //esconde o cursor
+    clean_screen(0);
 
     //ALOCAÇÕES
     mutex_lines = (pthread_mutex_t *)malloc(n_lines*sizeof(pthread_mutex_t));
@@ -97,9 +117,8 @@ int main(void) {
         for(i=0;i<n_threads;i++) pthread_join(threads[i], NULL);
     }
 
-    printf("\e[H"); //seta cursor pra posição inicial
-    printf("\e[J"); //limpa tudo a partir do cursor
-    printf("\e[?25h"); //exibe o cursor
+    printf("\e[0m"); //reseta cores para padrão
+    clean_screen(1);
 
     //FREE
     free(mutex_lines);
