1a743aaa3b4efeb74a5217cecf837a2654fe0d3d
Samuel Simões <samuelsimoes31@gmail.com>
2020-10-27 15:29:39 -0300

    Q2 - pequenas mudanças estéticasn o código

--- q2/questao2.c
+++ q2/questao2.c
@@ -4,10 +4,10 @@
 #include <unistd.h>
 #include <string.h>
 
-int n_lines, n_files, n_threads, open_file=0;
-pthread_mutex_t *mutex_lines = NULL;
-pthread_mutex_t mutex_open_file = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mutex_print = PTHREAD_MUTEX_INITIALIZER;
+int n_files, open_file=0;
+pthread_mutex_t *mutex_lines = NULL; //array de mutexes, uma para cada linha
+pthread_mutex_t mutex_open_file = PTHREAD_MUTEX_INITIALIZER; //mutex para anecessar variável "open_file" que diz qual o próximo arquivo a ser aberto
+pthread_mutex_t mutex_print = PTHREAD_MUTEX_INITIALIZER; //mutex para imprimir uma linha, pois o uso do cursosr é uma condição de disputa
 
 void set_line_color(int line){
     if(line != 6){
@@ -30,10 +30,10 @@ void set_line_color(int line){
 void print_line(int line, char *str, int len){
     int i=len;
     pthread_mutex_lock(&mutex_print);
-    printf("\e[%d;1H",line); //seta cursor pra linha "line" e coluna 1
+    printf("\e[%dH",line); //seta cursor pra linha "line" e coluna 1 (\e[%d;1H == \e[%dH)
     set_line_color(line);
     printf("%s",str); //imprime código e cidade
-    while (i++ < 20){ //espaço até a posição 20
+    while (i++ < 20){ //espaços até a posição 20
         putchar(' ');
     }
     printf("%s",str+len+1); //imprime hora
@@ -42,7 +42,7 @@ void print_line(int line, char *str, int len){
 }
 
 void clean_screen(int show_cursor){
-    printf("\e[H"); //seta cursor pra posição inicial
+    printf("\e[H"); //seta cursor pra posição inicial \e[H == \e[1;1H
     printf("\e[J"); //limpa tudo a partir do cursor
     printf("\e[?25%c",show_cursor?'h':'l'); //(?25h exibe, ?25l esconde) o cursor
 }
@@ -89,15 +89,22 @@ void *thread_func(){
 }
 
 int main(void) {
-    int i,rc;
+    int i,rc, n_lines, n_threads;
     pthread_t *threads = NULL;
     clean_screen(1);
-    printf("Número de arquivos:");
-    scanf("%d",&n_files);
-    printf("Número de linhas:");
-    scanf("%d",&n_lines);
-    printf("Número de número de threads:");
-    scanf("%d",&n_threads);
+    do{
+        printf("Número de arquivos a serem lidos (no mínimo 1):");
+        scanf("%d",&n_files);
+    } while(n_files < 1);
+    do{
+        printf("Número de linhas(1 à 7):");
+        scanf("%d",&n_lines);
+    } while(n_lines<1 || n_lines>7);
+    
+    do{
+        printf("Número de threads que irão ler os arquivos (1 à número de arquivos):");
+        scanf("%d",&n_threads);
+    } while(n_threads <1 && n_threads > n_files);
     clean_screen(0);
 
     //ALOCAÇÕES
